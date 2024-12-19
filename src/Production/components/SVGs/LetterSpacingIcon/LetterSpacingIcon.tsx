d0ae0ae12814f2dde132ae31137655afaa047c54
Mozes Stumpf <mozesstumpf@gmail.com>
2024-12-19 10:32:12 +0100

    viewBox: typo fix

--- src/Production/components/SVGs/LetterSpacingIcon/LetterSpacingIcon.tsx
+++ src/Production/components/SVGs/LetterSpacingIcon/LetterSpacingIcon.tsx
@@ -3,7 +3,7 @@ export const LetterSpacingIcon = () => {
 		<svg
 			width="16"
 			height="16"
-			viewBox="0 0 16 1 6"
+			viewBox="0 0 16 16"
 			fill="none"
 			xmlns="http://www.w3.org/2000/svg"
 		>
